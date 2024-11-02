511e2887796c8827e7e47a2f90de369c7e752863
cleame8922 <cleame8922@sungkyul.ac.kr>
2024-11-03 06:50:37 +0900

    chatbot fix

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 import './Nav.css';
 import { NavLink } from 'react-router-dom';
 import { IoPersonCircleSharp } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
 
 export default function Nav() {
     // 초기 로그인 상태를 로컬 스토리지 토큰으로 설정
+    const navigate = useNavigate();
     const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('accessToken'));
 
     // 로그아웃 함수
     const handleLogout = () => {
         localStorage.removeItem('accessToken'); // 토큰 삭제
         setIsLoggedIn(false); // 상태 업데이트
+        navigate('/');
     };
 
     return (
